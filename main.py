4e29e86f5ce30ed94d0c0827bd59a175f28a53a6
Williams <agbanagbawilliams@gmail.com>
2024-09-06 21:45:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd  
 import streamlit as st  # pip install streamlit
-import plotly-express
+import plotly.express as px
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
